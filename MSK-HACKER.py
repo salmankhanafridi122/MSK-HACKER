fd3ef1d89b74dfed52eccb6d137a81dd6aa8fc12
salmankhanafridi122 <95679020+salmankhanafridi122@users.noreply.github.com>
2022-01-03 22:53:02 +0500

    Create MSK-HACKER.py

---
+++ MSK-HACKER.py
@@ -0,0 +1,10 @@
+# pkg update
+# pkg upgrade
+# pkg install python
+# pkg install python2
+# pip2 install requests
+# pip2 install mechanize
+# pkg install git
+# git clone https://github.com/salmankhanafridi122/MSK-HACKER.git
+# cd MSK-HACKER
+# python2 MSK-HACKER.py
